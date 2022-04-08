b88f8a8f8f84102b69fab0f9db069f0a513be3d6
sundusardian <sundusardian@gmail.com>
2022-04-08 11:48:43 +0700

    add routing for Form Pekerjaan

--- src/App.js
+++ src/App.js
@@ -1,127 +1,124 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import { createBrowserHistory } from "history"
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
-import {GlobalProvider, GlobalContext} from './store/global/Provider'
+import { GlobalProvider, GlobalContext } from "./store/global/Provider";
 
-import Header from './components/Header'
-import DrawerNav from './components/menu/DrawerNav'
+import Header from "./components/Header";
+import DrawerNav from "./components/menu/DrawerNav";
 
+import "./styles/App.scss";
 
-import './styles/App.scss';
+import Login from "./views/auth/Login";
+import Error404 from "./views/error/404";
 
-import Login from './views/auth/Login'
-import Error404 from './views/error/404'
+import Home from "./views/home";
+import Profile from "./views/profile";
+import Prestasi from "./views/prestasi";
+import Certifikat from "./views/certificate";
+import Article from "./views/article/_index";
+import DetailPenilaianPejuang from "./views/DetailPenilaianPejuang";
+import FormPekerjaan from "./views/FormPekerjaan";
 
-import Home from './views/home'
-import Profile from './views/profile'
-import Prestasi from './views/prestasi'
-import Certifikat from './views/certificate'
-import Article from './views/article/_index'
+const PrivateRoute = function ({ children, ...rest }) {
+  const History = createBrowserHistory();
 
-
-
-const PrivateRoute = function({ children, ...rest }) {
-
-  const History = createBrowserHistory()
-
-  return (
-    !!localStorage.getItem('Token') 
-    ? (children) 
-    : (
-      History.push('/login'),
-      History.go()
-    )
-  )
-}
-
+  return !!localStorage.getItem("Token")
+    ? children
+    : (History.push("/login"), History.go());
+};
 
 export default function AppFunc() {
-
   return (
     <GlobalProvider>
       <BrowserRouter>
-      
-  
         <main>
           <Routes>
-
-            <Route exact
-              path='/login'
+            <Route
+              exact
+              path="/login"
               element={
-              localStorage.getItem('Token')
-                ? <Navigate
-                    to={{ pathname: `/`}}
-                  />
-                :<Login />
+                localStorage.getItem("Token") ? (
+                  <Navigate to={{ pathname: `/` }} />
+                ) : (
+                  <Login />
+                )
               }
             />
 
-            <Route exact
-              path='/'
+            <Route
+              exact
+              path="/"
               element={
                 <PrivateRoute>
-                  <Home/>
+                  <Home />
                 </PrivateRoute>
               }
             />
 
-<Route exact
-              path='/profile'
+            <Route
+              exact
+              path="/profile"
               element={
                 <PrivateRoute>
-                  <Profile/>
+                  <Profile />
                 </PrivateRoute>
               }
             />
 
-<Route exact
-              path='/prestasi'
+            <Route
+              exact
+              path="/prestasi"
               element={
                 <PrivateRoute>
-                  <Prestasi/>
+                  <Prestasi />
                 </PrivateRoute>
               }
             />
 
-<Route exact
-              path='/certifikat'
+            <Route
+              exact
+              path="/detail-penilaian/:id"
+              element={<DetailPenilaianPejuang />}
+            />
+
+            <Route exact path="/form-pekerjaan" element={<FormPekerjaan />} />
+
+            <Route
+              exact
+              path="/certifikat"
               element={
                 <PrivateRoute>
-                  <Certifikat/>
+                  <Certifikat />
                 </PrivateRoute>
               }
             />
 
-            {
-              [
-                '/article',
-                '/article/:type',
-                '/article/:type/:option'
-              ].map((path, index) => 
-                <Route key={index} path={path}
+            {["/article", "/article/:type", "/article/:type/:option"].map(
+              (path, index) => (
+                <Route
+                  key={index}
+                  path={path}
                   element={
                     <PrivateRoute>
-                      <Article/>
+                      <Article />
                     </PrivateRoute>
                   }
                 />
               )
-            }
+            )}
 
             <Route
-              path='*'
+              path="*"
               element={
                 <PrivateRoute>
-                  <Error404/>
+                  <Error404 />
                 </PrivateRoute>
               }
             />
-
-
           </Routes>
         </main>
       </BrowserRouter>
     </GlobalProvider>
-  )
+  );
 }
