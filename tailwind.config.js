635f6cbc40412cb1571d067dc90ed6dd1fcf798a
sundusardian <sundusardian@gmail.com>
2022-04-19 23:36:37 +0700

    modify class and add new custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,23 @@ module.exports = {
     },
 
     extend: {
-      color: {
+      colors: {
         primary: "#FFCD00",
+      },
+      minWidth: {
+        "11rem": "11.5rem",
+      },
+      spacing: {
+        "2.35rem": "2.35rem",
+      },
+      flex: {
+        "0-1": "0 1 100%",
+        2: "2 2 0%",
+        45: "0 1 45%",
+        48: "0 1 48%",
+        30: "0 1 30%",
       },
     },
   },
-  plugins: [
-  ],
-}
+  plugins: [],
+};
